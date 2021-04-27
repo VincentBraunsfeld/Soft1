3af8992f6c1d4a85c42d9d1f687966c821790c9f
badrguennoun <badrguennoun@badrs-mbp.fritz.box>
2021-04-27 18:52:26 +0200

    iswellSorted method wurde implementiert

--- Project1/src/Task.java
+++ Project1/src/Task.java
@@ -5,7 +5,27 @@ public class Task implements TaskIF {
 	public Task(String[][] s) {
 		zweiD = s;
 	}
+	public boolean isWellSorted(String[] sequenz) {
+        if (!kommtJederStringNurEinmalVor(sequenz)) {
+            return false;
+        }
+        if (!sindAlleWerteDerSequenzErlaubt(sequenz)) {
+            return false;
+        }
+        String fertig = "";
+        for (int i = 0; i < sequenz.length; i++) {
+            for (int j = 0; j < zweiD.length; j++) {
+                if (sequenz[i].equals(zweiD[j][1]) && !fertig.contains(zweiD[j][0])) {
+                    return false;
+                }
+            }
 
+            fertig += sequenz[i];
+        }
+
+        return true;
+
+    }
 	
 	public boolean kommtJederStringNurEinmalVor(String[] sequenz) {
 		String kommtJederStringNurEinmalVor = "";
