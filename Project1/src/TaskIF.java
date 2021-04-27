2809b2f5388d3e53020d28f2857590538dd7378c
badrguennoun <badrguennoun@badrs-mbp.fritz.box>
2021-04-27 18:20:57 +0200

    Erste Methode wurde implementiert

--- Project1/src/TaskIF.java
+++ Project1/src/TaskIF.java
@@ -1,8 +1,6 @@
 public interface TaskIF {
 
-    //public boolean isWellSorted(String[] sequenz);
     public boolean kommtJederStringNurEinmalVor(String[] sequenz);
-    //public boolean sindAlleWerteDerSequenzErlaubt(String[] sequenz);
 
 
 }
